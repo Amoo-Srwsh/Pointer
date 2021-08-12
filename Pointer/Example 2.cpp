cd7e15db8c026e400592300e10cbff0f694f5360
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-08-12 04:02:27 -0700

    Add files via upload

---
+++ Pointer/Example 2.cpp
@@ -0,0 +1,20 @@
+//Swap arrya index usign pointer
+//Git HUb Amoo-Srwsh
+
+#include <iostream>
+
+using namespace std;
+
+main()
+{
+    int a [5] = {5,4,3,2,1};
+    int b [5] = {1,2,3,4,5};
+    int *p;
+
+    for (int i = 0; b[i] != '\0'; i++)
+    {
+        p = b + i;
+        *(a + i) = *p;
+        cout << *(a + i) << "   ";
+    }
+}
