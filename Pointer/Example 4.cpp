8364b529d73c233c78f26716a0ee3278afc9fb15
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-08-16 07:35:09 -0700

    Add files via upload

---
+++ Pointer/Example 4.cpp
@@ -0,0 +1,28 @@
+//Amoo-Srwsh
+//Pointer - Example
+#include <iostream>
+using namespace std;
+
+int array_len (int a[])
+{
+	int i = 0; 
+	for (; a[i] != '\0'; i++);
+	return i;
+}
+main()
+{	
+	int a[] = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20};
+	int i = 0, j = 1, *pointer = a, index_array = array_len(a);
+			// pointer = first index
+
+	for (int l = 0; l < index_array/2; l++,i+=2,j+=2)
+	{
+		cout << (pointer + i) << "   " << (pointer + j) << endl;
+	}
+	i = 0; //Update (i)
+	j = 1; //Update (j)
+	for (int l = 0; l < index_array/2; l++,i+=2,j+=2)
+	{
+		cout << *(pointer + i) << "   " << *(pointer + j) << endl;
+	}
+}
