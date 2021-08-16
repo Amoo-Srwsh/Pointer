cbdfcd25bd43c5da22e5b88d9d8ff2679dcb75b8
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-08-16 13:57:40 -0700

    Add files via upload

---
+++ Pointer/Example 5.cpp
@@ -0,0 +1,19 @@
+//Amoo-Srwsh
+//array of pointer to arrays
+#include <iostream>
+using namespace std;
+
+main()
+{
+	int a[2] = {1,2};  // (a) using 2 element
+	int b[3] = {3,4,5};  // (b) using 3 element
+
+	int *p[] = {a,b};
+
+	cout << *(p[0]) << endl;
+	cout << *((p[0])+1) << endl;
+
+	cout << *(p[1]) << endl;
+	cout << *((p[1])+1) << endl;
+	cout << *((p[1])+2) << endl;
+}
