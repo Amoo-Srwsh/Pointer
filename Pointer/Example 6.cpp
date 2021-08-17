5d5213074754bd7fd7b621f6a2f536c2b94b1c36
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-08-17 06:28:59 -0700

    Add files via upload

---
+++ Pointer/Example 6.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a[] = {1,2,3};
+	int b[] = {6,7,8};
+	int c[] = {11,12,13};
+	int d[] = {16,17,18};
+
+	int *p[2][2] = {{a,b}
+		      ,{c,d}};
+
+	cout << *(*(*(p+0)+0)+0) << endl;
+	cout << *(*(*(p+0)+0)+1) << endl;
+	cout << *(*(*(p+0)+0)+2) << endl;
+
+	cout << *(*(*(p+0)+1)+0) << endl;
+	cout << *(*(*(p+0)+1)+1) << endl;
+	cout << *(*(*(p+0)+1)+2) << endl;
+
+	cout << *(*(*(p+1)+0)+0) << endl;
+	cout << *(*(*(p+1)+0)+1) << endl;
+	cout << *(*(*(p+1)+0)+2) << endl;
+
+	cout << *(*(*(p+1)+1)+0) << endl;
+	cout << *(*(*(p+1)+1)+1) << endl;
+	cout << *(*(*(p+1)+1)+2) << endl;
+
+}
