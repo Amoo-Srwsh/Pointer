8fb0aac60d1696a7225abeaadb9d932984652451
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-08-18 15:01:58 -0700

    Update Example 5.cpp

--- Pointer/Example 5.cpp
+++ Pointer/Example 5.cpp
@@ -8,7 +8,7 @@ main()
 	int a[2] = {1,2};  // (a) using 2 element
 	int b[3] = {3,4,5};  // (b) using 3 element
 
-	int *p[] = {a,b};
+	int *p[] = {a,b};    //p[] = {0x00,0x00};   //Example//
 
 	cout << *(p[0]) << endl;
 	cout << *((p[0])+1) << endl;
