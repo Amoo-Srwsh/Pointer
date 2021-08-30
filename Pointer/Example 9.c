44aca19052df02c3707c912427313bd80f214728
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-08-30 05:54:04 -0700

    Update Example 9.c

--- Pointer/Example 9.c
+++ Pointer/Example 9.c
@@ -1,1 +1,21 @@
 //Functions returning Pointer variables
+#include <stdio.h>
+
+int* larger(int*, int*);
+
+void main()
+{
+    int a = 15;
+    int b = 92;
+    int *p;
+    p = larger(&a, &b);
+    printf("%d is larger",*p);
+}
+
+int* larger(int *x, int *y)
+{
+    if(*x > *y)
+        return x;
+    else
+        return y;
+}
