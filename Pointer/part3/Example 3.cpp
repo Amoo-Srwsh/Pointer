bc5414f0027eb32c4f0b1f1a2356f987a1e15aed
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-09-26 12:42:22 -0400

    Add files via upload

---
+++ Pointer/part3/Example 3.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int *f (int *a1, int len1 ,int *a2 ,int len2 ,int pos);
+
+int main()
+{
+	int len1 = 3, len2 = 2;
+
+	int *a1 = new int [len1]; 
+	int *a2 = new int [len2];
+
+	cout << "\nArray 1:    ";
+	for (int i = 0; i < len1; i++){
+		a1[i] = rand()%100;
+		cout << a1[i] << "    ";
+	}
+
+	cout << "\nArrya 2:    ";
+	for (int i = 0; i < len2; i++){ 
+		a2[i] = rand()%100; 
+		cout << a2[i] << "    ";
+	}
+
+	int pos;
+	cout << "\nWhat position ? "; 
+	cin >> pos;
+
+	int *a3, len3;
+	len3 = len1 + len2; 
+	a3 = f(a1,len1,a2,len2,pos); 
+	
+	if (a3 == NULL)
+		cout << "\nInvalid position!\n";
+	else
+	{
+		cout << "New Array: ";
+		for (int i = 0; i < len3; i++) 
+			cout << *(a3 + i) << "    ";
+		cout << endl;
+	}
+	
+	delete []a1;
+	delete []a2;
+	delete []a3;
+}
+
+int *f (int *a1, int len1, int *a2, int len2, int pos)
+{
+	if (pos < 0 || pos >= len1)
+		return NULL;
+
+	int *a = new int[len1 + len2]; 
+
+	 int i = 0, j = 0, k = 0;
+
+	for (; i < pos; i++, k++)
+		*(a + i) = *(a1 + k);
+	for (; j < len2; ++j, ++k)
+		*(a + i) = *(a2 + k);
+	for (; i < len1; ++i, ++k)
+		*(a + i) = *(a1 + k); 
+	
+	return a; 
+}
