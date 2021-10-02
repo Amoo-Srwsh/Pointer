48c72543aa66b3e823ccac966e9025a238c1d236
Srwsh <85651275+Amoo-Srwsh@users.noreply.github.com>
2021-10-02 05:20:50 -0400

    Update Example 2.cpp

--- Pointer/part3/Example 2.cpp
+++ Pointer/part3/Example 2.cpp
@@ -1,40 +1,27 @@
-// Dynamic Array
-/* You can improve this code with a simple function (realloc) */
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	int x, size = 0, counter = 0; 
-	int *a = NULL;
-	int *p = NULL;
+        int x,counter = 0;
+        int *p = NULL;
 
-	while ( cin >> x )
-	{
-		if (counter >= size)
-		{
-			p = new int[size + 3];
-			
-			for (int i = 0; i < size; i++)
-				p[i] = a[i];
+        while ( cin >> x )
+        {
+                p = (int*) realloc (p, 1 * sizeof(int));
+                p[counter] = x;
+                counter ++;
+        }
+	
+        if (counter == 0)
+                cout << "Invalid input \n";
+        else
+        {
+                int sum = 0;
+                for (int i = 0; i < counter; i++)
+                        sum += p[i];
+                cout << "Average = " << (float) sum/counter << endl;
+        }
 
-			delete a;
-
-			a = p;
-			size += 3;
-		}
-		a[counter] = x;
-		counter ++;
-	}
-
-	if (counter == 0)
-		cout << "Invalid input \n";
-	else
-	{
-		int sum = 0;
-		for (int i = 0; i < counter; i++) 
-			sum += p[i];
-		cout << "Average = " << (float) sum/counter << endl;
-	}
-	delete a;
+        delete p;
 }
